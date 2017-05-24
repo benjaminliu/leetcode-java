d72569f9094845bc97a3f83476549de77aef9f31
BenjaminLiu <benjamin.liu@live.cn>
2017-05-24 11:21:47 +0800

    add some q

---
+++ src/main/java/com/test/leetcode/medium/LinkedListRandomNode.java
@@ -0,0 +1,41 @@
+package com.test.leetcode.medium;
+
+import com.test.leetcode.common.ListNode;
+
+import java.util.Random;
+
+/**
+ * Created by ben on 2017/5/24.
+ */
+public class LinkedListRandomNode {
+    private ListNode head;
+    private Random random;
+
+    /**
+     * @param head The linked list's head.
+     *             Note that the head is guaranteed to be not null, so it contains at least one node.
+     */
+    public LinkedListRandomNode(ListNode head) {
+        if (head == null) {
+            throw new IllegalArgumentException("head");
+        }
+        this.head = head;
+        random = new Random();
+    }
+
+    /**
+     * Returns a random node's value.
+     */
+    public int getRandom() {
+        ListNode temp = head;
+
+        int val = head.val;
+        for (int i = 1; temp != null; i++) {
+            if (random.nextInt(i) == 0) {
+                val = temp.val;
+            }
+            temp = temp.next;
+        }
+        return val;
+    }
+}
