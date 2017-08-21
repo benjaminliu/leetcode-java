07b87f7191b8c321b4bf46145506d0dd4a343d3e
BenjaminLiu <benjamin.liu@live.cn>
2017-08-21 12:00:49 +0800

    change 1 question

--- src/main/java/com/test/leetcode/easy/_374_GuessNumberHigherOrLower.java
+++ src/main/java/com/test/leetcode/easy/_374_GuessNumberHigherOrLower.java
@@ -6,30 +6,43 @@ package com.test.leetcode.easy;
 public class _374_GuessNumberHigherOrLower {
 
     public int guessNumber(int n) {
-        int start = 1;
-        int end = n;
-        int mid;
-        while (start <= end) {
-            mid = (start + end) / 2;
+        int l = 1, r = n;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
             int g = guess(mid);
-            if (g == 0) {
-                return mid;
-            } else if (g == 1) {
-                end = mid - 1;
+            if (g == 0) return mid;
+            else if (g == 1) {
+                l = mid + 1;
             } else {
-                start = mid + 1;
+                r = mid - 1;
             }
         }
-        return -1;
+        return 0;
+    }
+
+    public int guessNumber1(int n) {
+        int i = 1, j = n;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) == 1) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+        }
+        return i;
     }
 
     int guess(int num) {
-        if (num == 6) {
+        int compare = 1;
+        if (num == compare) {
             return 0;
-        } else if (num > 6) {
-            return 1;
-        } else {
+        } else if (num > compare) {
             return -1;
+        } else {
+            return 1;
         }
     }
 }
